be76d384df92ac0206615d81c990a1e5539cab23
Hai Pham Ngoc <ngochai285nd@gmail.com>
2024-12-26 20:41:58 +0700

    feat: update example

--- 01_basics/variables/src/main.rs
+++ 01_basics/variables/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    println!("The value of x is: {}", x);
+
+    /*
+    x = 6; // this is an error because x is immutable
+    println!("The value of x is: {}", x);
+     */
+
+    let mut y = 5; // mutable variable
+    y = 6;
+    println!("The value of y is: {}", y);
+
+    // constants are immutable by default
+    const TWO_HOURS_IN_SECONDS: u32 = 60 * 60 * 2;
+    println!("The value of TWO_HOURS_IN_SECONDS is: {}s", TWO_HOURS_IN_SECONDS);
 }
