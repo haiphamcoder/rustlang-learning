6b46c5ff90bdb929e05c2f5dd7f95b3f32249f4c
Hai Pham Ngoc <ngochai285nd@gmail.com>
2024-12-29 16:19:32 +0700

    feat: update references_borrowing

--- 02_ownership/references_borrowing/src/main.rs
+++ 02_ownership/references_borrowing/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("Hello, Rust!");
+    let len = calculate_length(&s1);
+    println!("The length of '{}' is {}.", s1, len);
+
+    let mut s2 = String::from("Hello! ");
+    change(&mut s2);
+    println!("s2 = {}", s2);
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn change(s: &mut String) {
+    s.push_str("You are welcome!");
 }
